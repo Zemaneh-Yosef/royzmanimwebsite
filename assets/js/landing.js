cf60437b97b808b1ed30288a18eac77502d2fb36
NightScript <18664762+NightScript370@users.noreply.github.com>
2024-09-23 17:04:27 -0400

    Fix scroll spy & Elyahu issues

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,50 +1,53 @@
 
-    var observer = new IntersectionObserver(onIntersection, {
-        root: null,   // default is the viewport
-        threshold: [0.1, 0.32, 0.52, 0.8, 1] // percentage of target's visible area. Triggers "onIntersection"
-    })
+var observer = new IntersectionObserver(onIntersection, {
+    root: null,   // default is the viewport
+    threshold: [0.1, 0.32, 0.52, 0.8, 1] // percentage of target's visible area. Triggers "onIntersection"
+})
 
-    // callback is called on intersection change
-    function onIntersection(entries, opts){
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        const logo = document.getElementById('logo');
-        if (!navbarCollapsible) {
+const navBarElem = document.getElementById('mainNav');
+const navBarCollapse = document.getElementById('navbarResponsive');
+const logo = document.getElementById('logo');
+
+// callback is called on intersection change
+function onIntersection(entries, opts){
+    if (!navBarElem) {
+        return;
+    }
+
+    entries.forEach(entry => {
+        if (entry.intersectionRatio == 1) {
+            navBarElem.classList.remove('navbar-landing-shrink')
+            navBarElem.classList.add('navbar-landing-expand')
+            navBarElem.setAttribute('data-bs-theme', 'light')
+            logo.classList.remove('stepO', 'stepTw', 'stepTh', 'stepF')
+
             return;
         }
 
-        entries.forEach(entry => {
-            if (entry.intersectionRatio == 1) {
-                navbarCollapsible.classList.remove('navbar-landing-shrink')
-                navbarCollapsible.classList.add('navbar-landing-expand')
-                navbarCollapsible.setAttribute('data-bs-theme', 'light')
-                logo.classList.remove('stepO', 'stepTw', 'stepTh', 'stepF')
+        navBarElem.classList.add('navbar-landing-shrink')
+        navBarElem.classList.remove('navbar-landing-expand')
+        navBarElem.removeAttribute('data-bs-theme')
 
-                return;
-            }
+        if (entry.intersectionRatio <= .1) {
+            logo.classList.add('stepF')
+            logo.classList.remove('stepO', 'stepTw', 'stepTh')
+        } else if (entry.intersectionRatio <= .32) {
+            logo.classList.add('stepTh')
+            logo.classList.remove('stepO', 'stepTw', 'stepF')
+        } else if (entry.intersectionRatio <= .52) {
+            logo.classList.remove('stepO', 'stepTh', 'stepF')
+            logo.classList.add('stepTw')
+        } else {
+            logo.classList.remove('stepTh', 'stepTw', 'stepF')
+            logo.classList.add('stepO')
+        }
+    })
+}
 
-            navbarCollapsible.classList.add('navbar-landing-shrink')
-            navbarCollapsible.classList.remove('navbar-landing-expand')
-            navbarCollapsible.removeAttribute('data-bs-theme')
+// Use the observer to observe an element
+observer.observe( document.getElementById('fakeLogo') )
 
-            if (entry.intersectionRatio <= .1) {
-                logo.classList.add('stepF')
-                logo.classList.remove('stepO', 'stepTw', 'stepTh')
-            } else if (entry.intersectionRatio <= .32) {
-                logo.classList.add('stepTh')
-                logo.classList.remove('stepO', 'stepTw', 'stepF')
-            } else if (entry.intersectionRatio <= .52) {
-                logo.classList.remove('stepO', 'stepTh', 'stepF')
-                logo.classList.add('stepTw')
-            } else {
-                logo.classList.remove('stepTh', 'stepTw', 'stepF')
-                logo.classList.add('stepO')
-            }
-        })
-    }
-
-    // Use the observer to observe an element
-    observer.observe( document.getElementById('fakeLogo') )
-
+window.addEventListener('DOMContentLoaded', event => {
     //  Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
@@ -55,15 +58,24 @@
         });
     };
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+    for (const modal of document.getElementsByClassName('modal')) {
+        modal.addEventListener('hide.bs.modal', () => logo.style.removeProperty('display'));
+        modal.addEventListener('show.bs.modal', () => logo.style.display = 'none');
+    }
+})
+
+// Collapse responsive navbar when toggler is visible
+const navbarToggler = document.body.querySelector('.navbar-toggler');
+const responsiveNavItems = [].slice.call(
+    document.querySelectorAll('#navbarResponsive .nav-link')
+);
+responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener('click', () => {
+        if (window.getComputedStyle(navbarToggler).display !== 'none') {
+            navbarToggler.click();
+        }
     });
+});
+
+navBarCollapse.addEventListener('show.bs.collapse', () => { logo.style.width = '30vw'; logo.style.top = '.5em'; })
+navBarCollapse.addEventListener('hide.bs.collapse', () => { ['width', 'top'].forEach(prop => logo.style.removeProperty(prop)); })
